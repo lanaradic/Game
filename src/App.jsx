fe66890dfb61f5d87482bd8e3fe576585ad11a1e
Lana Radic <lanaradic1990@gmail.com>
2023-09-20 10:26:25 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import boltl from "./assets/bolt-up-left.png";
+import boltr from "./assets/bolt-up-right.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [computerSelection, setComputerSelection] = useState(null);
+  const [userSelection, setUserSelection] = useState(null);
+  const [finalOutput, setFinalOutput] = useState(null);
+  const selection = ["🧱", "📰", "✂️", "🦎", "🖖"];
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+
+  // eslint-disable-next-line no-unused-vars
+  const score = (setFinalOutput) => {
+    if (setFinalOutput === "YOU WON!!! 🎉") {
+      setPlayerScore(playerScore + 1);
+    } else if (setFinalOutput === "YOU LOSE!!! ") {
+      setComputerScore(computerScore + 1);
+    }
+  };
+
+  const clickHandler = (value) => {
+    setUserSelection(value);
+    randomChoiceGenerator();
+  };
+
+  const randomChoiceGenerator = () => {
+    const randomSelection =
+      selection[Math.floor(Math.random() * selection.length)];
+    setComputerSelection(randomSelection);
+  };
+  const resetGame = () => {
+    setPlayerScore(null);
+    setComputerScore(null);
+    setFinalOutput(null);
+  };
+
+  useEffect(() => {
+    {
+      switch (userSelection + computerSelection) {
+        case "✂️📰":
+        case "🧱✂️":
+        case "📰🧱":
+        case "🦎📰":
+        case "🖖✂️":
+        case "🧱🦎":
+        case "📰🖖":
+        case "🖖🧱":
+        case "✂️🦎":
+        case "🦎🖖":
+          setFinalOutput("YOU WON!!! 🎉");
+          setPlayerScore(playerScore + 1);
+          break;
+        case "📰✂️":
+        case "✂️🧱":
+        case "🧱📰":
+        case "📰🦎":
+        case "✂️🖖":
+        case "🦎🧱":
+        case "🖖📰":
+        case "🧱🖖":
+        case "🦎✂️":
+        case "🖖🦎":
+          setFinalOutput("YOU LOSE!!! 👎 ");
+          setComputerScore(computerScore + 1);
+          break;
+        case "🧱🧱":
+        case "📰📰":
+        case "✂️✂️":
+        case "🦎🦎":
+        case "🖖🖖":
+          setFinalOutput("IT'S A DRAW!!! 💥 ");
+          break;
+      }
+    }
+  }, [computerSelection, userSelection]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="game">
+        <div className="game-box">
+          <div className="bolt-wrapper">
+            <img className="bolt-l" src={boltl} height="24px" alt="icon" />
+            <img className="bolt-r" src={boltr} height="24px" alt="icon" />
+          </div>
+          <div className="title-w">
+            <span>*Rock Paper Scissors Lizard Spock*</span>
+          </div>
+          <div className="score-wrapper">
+            <div className="show-score">SCORE: </div>
+            <div className="show-score">
+              {playerScore}:{computerScore}
+            </div>
+          </div>
+          <button onClick={resetGame}>Reset</button>
+          <div className="container">
+            <div className="section">
+              <div className="info">
+                <h4>You:</h4>
+              </div>
+              <div className="show">{userSelection}</div>
+            </div>
 
-export default App
+            <div className="section">
+              <div className="info">
+                <h4>Computer:</h4>
+              </div>
+              <div className="show computer">{computerSelection}</div>
+            </div>
+          </div>
+
+          <h2>{finalOutput} </h2>
+
+          <div className="attack-btn">
+            {selection.map((select, index) => (
+              <button key={index} onClick={() => clickHandler(select)}>
+                {select}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
