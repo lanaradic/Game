804afd67c5d5b1d6b5abcc08a3e01ad23d6f5c23
Lana Radic <lanaradic1990@gmail.com>
2023-10-04 08:11:00 +0200

    css-update-game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
-import boltl from "./assets/bolt-up-left.png";
-import boltr from "./assets/bolt-up-right.png";
 
 const App = () => {
   const [computerSelection, setComputerSelection] = useState(null);
@@ -80,10 +78,6 @@ const App = () => {
     <>
       <div className="game">
         <div className="game-box">
-          <div className="bolt-wrapper">
-            <img className="bolt-l" src={boltl} height="24px" alt="icon" />
-            <img className="bolt-r" src={boltr} height="24px" alt="icon" />
-          </div>
           <div className="title-w">
             <span>*Rock Paper Scissors Lizard Spock*</span>
           </div>
